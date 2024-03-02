7c82a1d0c92cbaa7916f625154ec4cc84fb9dc74
takashi823 <uchimura.takashi724@gmail.com>
2024-03-02 15:06:49 +0900

    add: timelineの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { SampleModal } from "@/components/SampleModal/SampleModal";
 import { SampleTabs } from "@/components/SampleTabs/SampleTabs";
+import { SampleTimeline } from "@/components/SampleTimeline/SampleTimeline";
 
 export default function Home() {
   return (
     <div>
       <SampleModal />
       <SampleTabs />
+      <SampleTimeline />
     </div>
   );
 }
---
+++ src/components/SampleTimeline/SampleTimeline.tsx
@@ -0,0 +1,35 @@
+"use client"
+import * as React from 'react';
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+
+export function SampleTimeline() {
+  return (
+    <Timeline>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Eat</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Code</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+        </TimelineSeparator>
+        <TimelineContent>Sleep</TimelineContent>
+      </TimelineItem>
+    </Timeline>
+  );
+}
